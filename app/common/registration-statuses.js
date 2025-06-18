2a15b9534927376b215354fcabc23d15541656f7
ishai zilberman <ishainwa@gmail.com>
2025-06-18 13:23:37 +0300

    DELETE:Remove unused Registration Endpoints

--- app/common/registration-statuses.js
+++
@@ -1,9 +0,0 @@
-const registrationStatuses = Object.freeze({
-    PENDING : "pending",
-    APPROVED : "approved",
-    DECLINED : "declined",
-    CANCELLED : "cancelled",
-    PARTICIPATED : "participated",
-})
-
-module.exports = registrationStatuses;
